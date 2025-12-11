157e0090d8f6dd75bea1ea006612ec6ae919e69a
Jaco Labuschagne <zodimo@gmail.com>
2025-12-11 13:13:38 +0200

    demo update

--- cmd/demo/ui.go
+++ cmd/demo/ui.go
@@ -7,10 +7,12 @@ import (
 	"go-compose-dev/compose/foundation/layout/row"
 	"go-compose-dev/compose/foundation/material/button"
 	m3Button "go-compose-dev/compose/foundation/material3/button"
+	"go-compose-dev/compose/ui/graphics/shape"
 
 	"go-compose-dev/compose/foundation/text"
 	"go-compose-dev/internal/modifiers/background"
 	"go-compose-dev/internal/modifiers/clickable"
+	"go-compose-dev/internal/modifiers/clip"
 	"go-compose-dev/internal/modifiers/padding"
 	"go-compose-dev/internal/modifiers/size"
 	"go-compose-dev/internal/modifiers/weight"
@@ -39,7 +41,7 @@ func UI(c api.Composer) api.LayoutNode {
 					})),
 					column.WithModifier(weight.Weight(1)),
 					column.WithModifier(background.Background(color.NRGBA{R: 0, G: 0, B: 200, A: 200})),
-					column.WithModifier(padding.PaddingAll(20)),
+					column.WithModifier(padding.All(20)),
 					column.WithModifier(background.Background(color.NRGBA{R: 0, G: 100, B: 200, A: 200})),
 				),
 				column.Column(
@@ -48,12 +50,19 @@ func UI(c api.Composer) api.LayoutNode {
 					column.WithModifier(background.Background(color.NRGBA{R: 150, G: 0, B: 0, A: 200})),
 				),
 				column.Column(
-					compose.Sequence(),
+					compose.Sequence(
+						button.Button(func() {
+							counterCell.Set(counterCell.Get().(int) + 1)
+							fmt.Println("Button clicked!")
+						}, "click me",
+							button.WithModifier(size.FillMax())),
+					),
 					column.WithModifier(size.Size(50, 50)),
 					column.WithModifier(background.Background(color.NRGBA{R: 100, G: 0, B: 0, A: 200})),
 					column.WithModifier(clickable.OnClick(func() {
 						fmt.Println("Last Column clicked!!")
 					})),
+					column.WithModifier(clip.Clip(shape.ShapeCircle)),
 				),
 			),
 				row.WithModifier(size.Size(500, 300)),
@@ -66,13 +75,10 @@ func UI(c api.Composer) api.LayoutNode {
 					text.StyleWithColor(color.NRGBA{R: 255, G: 255, B: 255, A: 255}),
 				),
 				text.WithModifier(background.Background(color.NRGBA{R: 100, G: 0, B: 0, A: 150})),
-				text.WithModifier(padding.PaddingAll(20)),
+				text.WithModifier(padding.All(20)),
 				text.WithModifier(background.Background(color.NRGBA{R: 200, G: 0, B: 50, A: 50})),
 			),
-			button.Button(func() {
-				counterCell.Set(counterCell.Get().(int) + 1)
-				fmt.Println("Button clicked!")
-			}, "click me"),
+
 			m3Button.Text(func() {
 				fmt.Println("M3 Text Button clicked!")
 			}, "Hello M3 Text Button"),
