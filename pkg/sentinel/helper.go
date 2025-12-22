766df83f220d8806894ad022090fa9dc01de86b8
Jaco Labuschagne <zodimo@gmail.com>
2025-12-22 22:33:44 +0200

    Sentinel

--- pkg/sentinel/helper.go
+++ pkg/sentinel/helper.go
@@ -1,13 +1,27 @@
-package sentinel
+package Sentinel
 
+// Do not implement this interface on complex objects
+// value classes inline to primitives, and Unspecified is just the zero bit pattern.
 type Sentinel interface {
 	IsSpecified() bool
+	neverImplement()
 }
 
 // Generic TakeOrElse for any type with IsSpecified() method
-func TakeOrElse[T interface{ Sentinel }](a, b, unspecified T) T {
+// golang interface or value receiver
+// DO NOT PASS NIL as arguments
+func TakeOrElseValue[T interface{ IsSpecified() bool }](a, b T) T {
 	if a.IsSpecified() {
 		return a
 	}
 	return b
+}
+
+// Generic TakeOrElse for any type with IsSpecified() method
+// golang interface or pointer receiver
+func TakeOrElsePointer[T any](a, b *T) *T {
+	if a == nil {
+		return b
+	}
+	return a
 }
