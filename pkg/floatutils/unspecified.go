b7400e30ee6bdbeb536743bdb70478a0cab3ba63
Jaco Labuschagne <zodimo@gmail.com>
2025-12-23 07:31:02 +0200

    FloatUnspecified

---
+++ pkg/floatutils/unspecified.go
@@ -0,0 +1,14 @@
+package floatutils
+
+import "math"
+
+type Float interface {
+	~float32 | ~float64
+}
+
+var FloatUnspecified = math.NaN()
+
+func IsSpecified[T Float](f T) bool {
+	//same as !math.IsNaN()
+	return float64(f) != FloatUnspecified
+}
