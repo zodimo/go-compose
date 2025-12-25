06a967b62af893b455dac8f8012dde30f1a1320a
Jaco Labuschagne <zodimo@gmail.com>
2025-12-25 09:32:18 +0200

    update message

--- compose/ui/text/style/align_test.go
+++ compose/ui/text/style/align_test.go
@@ -70,7 +70,7 @@ func TestTextAlign_IsSpecified(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.align.IsTextAlign(); got != tt.expected {
-				t.Errorf("TextAlign%s.IsSpecified() = %v, want %v", tt.name, got, tt.expected)
+				t.Errorf("TextAlign%s.IsTextAlign() = %v, want %v", tt.name, got, tt.expected)
 			}
 		})
 	}
