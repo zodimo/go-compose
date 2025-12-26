8d14d92ca648d73f1f0e7253703153d8cb38bde8
Jaco Labuschagne <zodimo@gmail.com>
2025-12-26 14:54:35 +0200

    spanstyle progress

--- compose/ui/text/span_style.go
+++ compose/ui/text/span_style.go
@@ -5,19 +5,39 @@ import (
 	"github.com/zodimo/go-compose/compose/ui/text/font"
 	"github.com/zodimo/go-compose/compose/ui/text/intl"
 	"github.com/zodimo/go-compose/compose/ui/text/style"
+	"github.com/zodimo/go-compose/compose/ui/unit"
 )
+
+var SpanStyleUnspecified *SpanStyle = &SpanStyle{
+	textForegroundStyle:    nil,
+	FontSize:               unit.TextUnitUnspecified,
+	FontWeight:             font.FontWeightUnspecified,
+	FontStyle:              font.FontStyleUnspecified,
+	FontSynthesis:          nil,
+	FontFamily:             nil,
+	FontFeatureSettings:    "",
+	LetterSpacing:          unit.TextUnitUnspecified,
+	BaselineShift:          style.BaselineShiftUnspecified,
+	TextGeometricTransform: nil,
+	LocaleList:             nil,
+	Background:             graphics.ColorUnspecified,
+	TextDecoration:         nil,
+	Shadow:                 nil,
+	PlatformStyle:          nil,
+	DrawStyle:              nil,
+}
 
 var _ Annotation = (*SpanStyle)(nil)
 
 type SpanStyle struct {
-	textForegroundStyle    style.TextForegroundStyle
-	FontSize               FontSize
+	textForegroundStyle    *style.TextForegroundStyle
+	FontSize               unit.TextUnit
 	FontWeight             font.FontWeight
 	FontStyle              font.FontStyle
 	FontSynthesis          *font.FontSynthesis
 	FontFamily             font.FontFamily
 	FontFeatureSettings    string
-	LetterSpacing          uiTextUnit
+	LetterSpacing          unit.TextUnit
 	BaselineShift          style.BaselineShift
 	TextGeometricTransform *style.TextGeometricTransform
 	LocaleList             *intl.LocaleList
@@ -32,17 +52,13 @@ func (s SpanStyle) isAnnotation() {}
 
 // Props
 func (s SpanStyle) Color() graphics.Color {
-	return s.textForegroundStyle.Color()
+	return s.textForegroundStyle.Color
 }
 func (s SpanStyle) Brush() graphics.Brush {
-	return s.textForegroundStyle.Brush()
+	return s.textForegroundStyle.Brush
 }
 func (s SpanStyle) Alpha() float32 {
-	return s.textForegroundStyle.Alpha()
-}
-
-func (s SpanStyle) Merge(other *SpanStyle) *SpanStyle {
-	panic("SpanStyle Merge not implemented")
+	return s.textForegroundStyle.Alpha
 }
 
 func (s SpanStyle) Copy() *SpanStyle {
@@ -51,4 +67,28 @@ func (s SpanStyle) Copy() *SpanStyle {
 
 func (s SpanStyle) ToString() string {
 	panic("SpanStyle ToString not implemented")
+}
+
+func MergeSpanStyle(a, b *SpanStyle) *SpanStyle {
+	a = CoalesceSpanStyle(a, SpanStyleUnspecified)
+	b = CoalesceSpanStyle(b, SpanStyleUnspecified)
+
+	if a == SpanStyleUnspecified {
+		return b
+	}
+	if b == SpanStyleUnspecified {
+		return a
+	}
+
+	// Both are custom: allocate new merged style
+	return &SpanStyle{
+		textForegroundStyle: style.MergeTextForegroundStyle(a.textForegroundStyle, b.textForegroundStyle),
+	}
+}
+
+func CoalesceSpanStyle(ptr, def *SpanStyle) *SpanStyle {
+	if ptr == nil {
+		return def
+	}
+	return ptr
 }
