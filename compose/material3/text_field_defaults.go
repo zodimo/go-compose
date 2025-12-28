3d0a5e72b39882a23e0a82a06da109c81a1d4af3
Jaco Labuschagne <zodimo@gmail.com>
2025-12-28 11:43:51 +0200

    material3 TextFieldColors

---
+++ compose/material3/text_field_defaults.go
@@ -0,0 +1,54 @@
+package material3
+
+import (
+	"github.com/zodimo/go-compose/compose/foundation/next/text/selection"
+	"github.com/zodimo/go-compose/compose/ui/graphics"
+)
+
+type Color = graphics.Color
+
+type TextFieldColors struct {
+	FocusedTextColor             Color
+	UnfocusedTextColor           Color
+	DisabledTextColor            Color
+	ErrorTextColor               Color
+	FocusedContainerColor        Color
+	UnfocusedContainerColor      Color
+	DisabledContainerColor       Color
+	ErrorContainerColor          Color
+	CursorColor                  Color
+	ErrorCursorColor             Color
+	TextSelectionColors          selection.TextSelectionColors
+	FocusedIndicatorColor        Color
+	UnfocusedIndicatorColor      Color
+	DisabledIndicatorColor       Color
+	ErrorIndicatorColor          Color
+	FocusedLeadingIconColor      Color
+	UnfocusedLeadingIconColor    Color
+	DisabledLeadingIconColor     Color
+	ErrorLeadingIconColor        Color
+	FocusedTrailingIconColor     Color
+	UnfocusedTrailingIconColor   Color
+	DisabledTrailingIconColor    Color
+	ErrorTrailingIconColor       Color
+	FocusedLabelColor            Color
+	UnfocusedLabelColor          Color
+	DisabledLabelColor           Color
+	ErrorLabelColor              Color
+	FocusedPlaceholderColor      Color
+	UnfocusedPlaceholderColor    Color
+	DisabledPlaceholderColor     Color
+	ErrorPlaceholderColor        Color
+	FocusedSupportingTextColor   Color
+	UnfocusedSupportingTextColor Color
+	DisabledSupportingTextColor  Color
+	ErrorSupportingTextColor     Color
+	FocusedPrefixColor           Color
+	UnfocusedPrefixColor         Color
+	DisabledPrefixColor          Color
+	ErrorPrefixColor             Color
+	FocusedSuffixColor           Color
+	UnfocusedSuffixColor         Color
+	DisabledSuffixColor          Color
+	ErrorSuffixColor             Color
+}
