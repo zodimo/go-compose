260929b20f1c455af7d392475bc1093c860e0341
Jaco Labuschagne <zodimo@gmail.com>
2026-01-01 09:37:40 +0200

    some sentinel helpers

--- compose/ui/text/text_style.go
+++ compose/ui/text/text_style.go
@@ -7,10 +7,26 @@ import (
 	"github.com/zodimo/go-compose/compose/ui/unit"
 )
 
+var TextStyleUnspecified *TextStyle = &TextStyle{
+	lineBreak: style.LineBreakUnspecified,
+}
+
 var _ TextStyleInterface = (*TextStyle)(nil)
 
 type TextStyle struct {
 	font       font.Font
+	fontSize   unit.TextUnit
+	fontWeight font.FontWeight
+	fontStyle  font.FontStyle
+
+	fontFamily font.FontFamily
+
+	textDecoration *style.TextDecoration
+	letterSpacing  unit.TextUnit
+
+	textAlign     style.TextAlign
+	textDirection style.TextDirection
+
 	color      graphics.Color
 	background graphics.Color
 	lineHeight unit.TextUnit
@@ -27,25 +43,25 @@ func (ts TextStyle) Color() graphics.Color {
 	return ts.color
 }
 func (ts TextStyle) FontFamily() font.FontFamily {
-	panic("FontFamily not implemented")
+	return ts.fontFamily
 }
 func (ts TextStyle) FontFeatureSettings() string {
 	panic("FontFeatureSettings not implemented")
 }
 func (ts TextStyle) FontSize() unit.TextUnit {
-	panic("FontSizenot implemented")
+	return ts.fontSize
 }
 func (ts TextStyle) FontStyle() font.FontStyle {
-	panic("FontStyle not implemented")
+	return ts.fontStyle
 }
 func (ts TextStyle) FontSynthesis() *font.FontSynthesis {
 	panic("FontSynthesis not implemented")
 }
 func (ts TextStyle) FontWeight() font.FontWeight {
-	panic("FontWeightnot implemented")
+	return ts.fontWeight
 }
 func (ts TextStyle) LetterSpacing() unit.TextUnit {
-	panic("LetterSpacing not implemented")
+	return ts.letterSpacing
 }
 
 func (ts TextStyle) LineBreak() style.LineBreak {
@@ -55,18 +71,63 @@ func (ts TextStyle) LineHeight() unit.TextUnit {
 	return ts.lineHeight
 }
 func (ts TextStyle) TextAlign() style.TextAlign {
-	panic("TextAlign not implemented")
+	return ts.textAlign
 }
 func (ts TextStyle) TextDecoration() *style.TextDecoration {
-	panic("TextDecoration not implemented")
+	return ts.textDecoration
 }
 func (ts TextStyle) TextDirection() style.TextDirection {
-	panic("TextDirection not implemented")
+	return ts.textDirection
 }
 func (ts TextStyle) ToString() string {
 	panic("ToString not implemented")
 }
 
 func TextStyleResolveDefaults(ts *TextStyle, direction unit.LayoutDirection) *TextStyle {
-	return &TextStyle{}
+	ts = CoalesceTextStyle(ts, TextStyleUnspecified)
+	return &TextStyle{
+
+		fontSize:   ts.FontSize().TakeOrElse(DefaultFontSize),
+		fontWeight: ts.FontWeight().TakeOrElse(font.FontWeightNormal),
+		fontStyle:  ts.FontStyle().TakeOrElse(font.FontStyleNormal),
+		// FontSynthesis:          font.TakeOrElseFontSynthesis(ss.FontSynthesis, font.FontSynthesisAll),
+		fontFamily: font.TakeOrElseFontFamily(ts.FontFamily(), font.FontFamilyDefault),
+		// FontFeatureSettings:    sentinel.TakeOrElseString(ss.FontFeatureSettings, ""),
+		letterSpacing: ts.LetterSpacing().TakeOrElse(DefaultLetterSpacing),
+		// BaselineShift:          style.TakeOrElseBaselineShift(ss.BaselineShift, style.BaselineShiftNone),
+		// TextGeometricTransform: style.TakeOrElseTextGeometricTransform(ss.TextGeometricTransform, style.TextGeometricTransformNone),
+		// LocaleList:             intl.TakeOrElseLocaleList(ss.LocaleList, nil), //LocaleList.Current - local provider
+		background:     ts.Background().TakeOrElse(DefaultBackgroundColor),
+		textDecoration: style.TakeOrElseTextDecoration(ts.TextDecoration(), style.TextDecorationNone),
+		// Shadow:                 graphics.TakeOrElseShadow(ss.Shadow, graphics.ShadowNone),
+		// PlatformStyle:          ss.PlatformStyle,
+		// DrawStyle:              graphics.TakeOrElseDrawStyle(ss.DrawStyle, graphics.DrawStyleFill),
+
+		textAlign:     ts.TextAlign().TakeOrElse(style.TextAlignStart),
+		textDirection: style.ResolveTextDirection(direction, ts.TextDirection()),
+		lineHeight:    ts.LineHeight().TakeOrElse(unit.TextUnitUnspecified),
+		// TextIndent:      style.TakeOrElseTextIndent(ps.TextIndent, style.TextIndentNone),
+		// PlatformStyle:   ps.PlatformStyle,
+		// LineHeightStyle: ps.LineHeightStyle,
+		lineBreak: ts.LineBreak().TakeOrElse(style.LineBreakSimple),
+		// Hyphens:         ps.Hyphens.TakeOrElse(style.HyphensNone),
+		// TextMotion:      style.TakeOrElseTextMotion(ps.TextMotion, style.TextMotionStatic),
+	}
+}
+
+func IsSpecifiedTextStyle(s *TextStyle) bool {
+	return s != nil && s != TextStyleUnspecified
+}
+func TakeOrElseTextStyle(s, def *TextStyle) *TextStyle {
+	if !IsSpecifiedTextStyle(s) {
+		return def
+	}
+	return s
+}
+
+func CoalesceTextStyle(ptr, def *TextStyle) *TextStyle {
+	if ptr == nil {
+		return def
+	}
+	return ptr
 }
